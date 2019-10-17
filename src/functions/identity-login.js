040e28a1afbe0b7b9a161fdaf6c2f1b696696930
basilcea <ogbonnabasil3@gmail.com>
2019-10-17 15:07:49 +0100

    add dot-env

--- src/functions/identity-login.js
+++ src/functions/identity-login.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-expressions */
 import nodemailer from "nodemailer";
-
+import dotenv from 'dot-env';
+dotenv.config()
 const sender = process.env.EMAIL;
 const pass = process.env.PASSWORD;
+const host = process.env.host;
+const port = process.env.port
 
 exports.handler = async (event, context) => {
   try {
     const { user } = context.clientContext;
     const { data } = JSON.parse(event.body);
     const transporter = nodemailer.createTransport({
-      host: "mail.privateemail.com",
-      port: 587,
+      host,
+      port,
       secure: false,
       auth: {
         user,
